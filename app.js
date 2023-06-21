29286592630b663d08664fc833209c9fa31b75f9
Alexis Winters <alexiscodes@yahoo.com>
2023-06-21 10:47:58 -0500

    File Upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
 const mongoose = require("mongoose");
 
